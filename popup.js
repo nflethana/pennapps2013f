bb027812c392ed557d8428e0d8e2aafad20115d4
Spencer Moran <smoran02@gmail.com>
2013-09-07 15:44:46 -0700

    rename buttons

--- popup.js
+++ popup.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	});
 
-	$('.deleteX').click(function(){
+	$('.anchorX').click(function(){
 		console.log('clicked this shit');
 	});
 
