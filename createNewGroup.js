298f58158b8b69d4a86eead471d107129d26ef0e
Ethan Abramson <nflethana@gmail.com>
2013-09-07 04:40:36 -0400

    working add button

--- createNewGroup.js
+++ createNewGroup.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-	console.log("blah");
-	$("body").append("<p> YOOOO </p>");
+
   $('#finish').click(function() {
     var input = $('#groupName');
 	$('body').append($("p").text(input.val()));
+	chrome.runtime.getBackgroundPage(function(page) {
+		page.addCategory(input.val());
+		console.log(page.categories);
+	});
   });
 });
