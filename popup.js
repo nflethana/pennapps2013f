3450d8bcf26b7cfe3417e1727911027a16480ca5
Ethan Abramson <nflethana@gmail.com>
2013-09-07 02:44:11 -0400

    refactoring

--- popup.js
+++ popup.js
@@ -1,12 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-  $('#foo').click(function() {
+  $('#createNewGroup').click(function() {
     createNewGroup();
-    console.log("yoooo");
   });
-  console.log('wooooooo');
-  chrome.extension.getBackgroundPage().console.log('foo');
 });
 
 function createNewGroup() {
-  self.location="../test.html";
+  self.location="../createNewGroup.html";
 }
