74ebb34f1a14ce2c15e446f1665e6de3cceb4f3e
Alex Wissmann <awiss@wharton.upenn.edu>
2013-09-07 02:16:22 -0400

    merged

--- popup.js
+++ popup.js
@@ -2,9 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
   var ptag = document.createElement('p');
   ptag.innerHTML = "skl;sdfjlasl;kdfjsadl;k";
   document.body.appendChild(ptag);
-<<<<<<< HEAD
-});
-=======
   console.log('wooooooo');
 });
->>>>>>> b69235b943854b0d8617f7188e27890567891ff3
+
