7662e8c29ded005782dc0ea1dd5e6f68d2a9ab45
Alex Wissmann <awiss@wharton.upenn.edu>
2013-09-07 12:52:12 -0400

    adding checkboxes

--- popup.js
+++ popup.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
   chrome.runtime.getBackgroundPage(function(page){
     chrome.tabs.getSelected(function(tab){
-      for(category in page.categories){
-        console.log(category);    
-      }    
-        //chrome.tabs.remove(tab.id);
-      
-      
-    });
 
-    $('#createNewGroup').click(function() {
-       createNewGroup();
-   });
+    });
+    console.log(page.categories);
+    var $list = $('#group-list');
+    for (var i=0;i<page.categories.length;i++){
+      var li = $('li');
+      li.append('<input type="checkbox" name="'+page.categories[i]+'">');
+      li.append('<label>'+page.categories[i]+'</label>');
+      $list.append(li);
+    }
+      $('#createNewGroup').click(function() {
+         createNewGroup();
+     });
   });
 });
 
